8a0ee8e7a7a10b586b3143e74d2c0448f0fd9763
Moataz Soliman <msolima91@gmail.com>
2015-11-20 01:13:57 -0500

    privacy and terms of use

--- materiallogindemo-master/app/src/main/java/com/valyria/uhungry/SignupActivity.java
+++ materiallogindemo-master/app/src/main/java/com/valyria/uhungry/SignupActivity.java
@@ -3,9 +3,12 @@ package com.valyria.uhungry;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +45,12 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.inject(this);
+
+        TextView textView =(TextView)findViewById(R.id.textView1);
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "By clicking Sign Up, you agree to our <a href='http://uhungryottawa.appspot.com/terms'>Terms of Use</a> and our <a href='http://uhungryottawa.appspot.com/privacy'>Privacy Policy</a>";
+        textView.setText(Html.fromHtml(text));
 
         _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -164,6 +173,17 @@ public class SignupActivity extends AppCompatActivity {
         finish();
 
 //        finish();
+    }
+
+    public void terms(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://http://uhungryottawa.appspot.com/terms"));
+        startActivity(intent);
+    }
+    public void privacy(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://http://uhungryottawa.appspot.com/privacy"));
+        startActivity(intent);
     }
 
     public void onSignupFailed() {
